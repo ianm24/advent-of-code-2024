146b148f615cd309eb668430c14f256681f71946
Ian Chandler McDowell <ianmcdowell24@gmail.com>
2024-11-28 14:13:54 -0500

    Made template in preparation for the start of advent

---
+++ template/boilerplate.py
@@ -0,0 +1,43 @@
+"""Module containing boilerplate code to be used for each day of advent."""
+
+DATA_FILEPATH = "./"
+
+# =============================Helper Functions START==========================
+
+
+def read_data_file(filename):
+    """Reads the data file and returns a list of strings for each line."""
+    data = None
+    with open(DATA_FILEPATH+filename, "r") as data_file:
+        lines = data_file.readlines()
+
+        # Need to remove unneeded new line characters
+        for i, line in enumerate(lines):
+            line = line.replace("\n", "")
+            lines[i] = line
+
+        data = lines
+
+    return data
+
+# =============================Helper Functions END============================
+
+
+def get_solution(input_data):
+    """Takes the input data and returns the solution."""
+    # Put the actual logic here
+    output_data = input_data
+    return output_data
+
+
+def main():
+    """Imports data then gets and prints the solution."""
+    data_filename = "data.txt"
+    input_data = read_data_file(data_filename)
+    solution = get_solution(input_data)
+
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
